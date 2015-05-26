db8ca5b85f4fc5f88669117ca5a125a4cd885b02
Lamin Sanneh <lamin.evra@gmail.com>
2015-05-26 10:31:05 +0000

    Fixed some bugs related to handling events

--- client/app/components/inline-model-creator.js
+++ client/app/components/inline-model-creator.js
@@ -3,15 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   inEditMode: false,
   title: '',
-  click: function(){
-    this.set('inEditMode', true);
-    this.set('isFocussed', true);
-  },
   actions: {
     createNewModel: function(title, modelType, parentModelType, parentId){
       this.container.lookup('route:application').send(this.get('action'), title, modelType, parentModelType,parentId);
       this.set('title', '');
       this.set('inEditMode', false);
+    },
+    openForm: function(){
+      this.set('inEditMode', true);
+      this.set('isFocussed', true);
     },
     closeForm: function(){
       console.log('Lost Focus');
--- client/app/components/inline-model-editor.js
+++ client/app/components/inline-model-editor.js
@@ -3,18 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   inEditMode: false,
   title: '',
-  click: function(){
-    this.set('inEditMode', true);
-    this.set('isFocussed', true);
-    this.set('title', this.get('modelItem.title'));
-  },
   actions: {
     updateModel: function(title, model){
       this.container.lookup('route:application').send(this.get('action'), title, model);
       this.set('title', '');
       this.set('inEditMode', false);
     },
-    lostFocus: function(){
+    openForm: function(){
+      this.set('inEditMode', true);
+      this.set('isFocussed', true);
+      this.set('title', this.get('modelItem.title'));
+    },
+    closeForm: function(){
       console.log('Lost Focus');
       this.set('inEditMode', false);
     }
